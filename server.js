ced02f82f2ac77008c0814ab7fbeea02bec55e2b
Henry <hencan.1997@gmail.com>
2021-07-12 09:31:34 -0400

    Expressrewrite started

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
-var http = require('http');
-var fs = require('fs'); 
-var url = require('url');
+const express = require('express');
+const app = express();
+const port = 8080;
 
-http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
-  if (err) {
-  res.writeHead(404, {'Content-type': 'text/html'});
-  return res.end("404 Not Found");
-  }
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(data);
-  return res.end();
-});
-}).listen(8080);
+app.get('/', (req,res) => {
+res.send("Hello world!")
+})
+
+app.listen(port, () => {
+console.log("App is listening on port 8080!")
+})
